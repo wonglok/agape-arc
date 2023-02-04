922ef5d45537f8dcb733874e7accdbe3e7d7eb8d
Wong Lok <wonglok@users.noreply.github.com>
2023-02-04 12:28:00 +0800

    style

--- src/http/get-index/index.mjs
+++ src/http/get-index/index.mjs
@@ -16,7 +16,6 @@ export async function handler(req) {
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <title>Welcome</title>
-      ${style}
       <style>
          * { margin: 0; padding: 0; box-sizing: border-box; } body { font-family: -apple-system, BlinkMacSystemFont, sans-serif; }
       </style>
