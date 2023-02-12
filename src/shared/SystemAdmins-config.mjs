828592023ea9b9492f1a99902e8ce7acef047e4c
Wong Lok <wonglok@users.noreply.github.com>
2023-02-12 11:55:34 +0800

    save

--- src/shared/SystemAdmins-config.mjs
+++ src/shared/SystemAdmins-config.mjs
@@ -4,6 +4,12 @@ export const SystemAdmins = [
     type: "metamask",
     userID: "0x15cA2Eee6774E363fA5bbfA9E44120B89e4C7c1B",
   },
+  {
+    name: "lok-agape",
+    type: "metamask",
+    userID: "0xeCB732CffbF1d34d9eEF4aB09ddF5237593deD19",
+  },
+  //
 
   //
 ];
